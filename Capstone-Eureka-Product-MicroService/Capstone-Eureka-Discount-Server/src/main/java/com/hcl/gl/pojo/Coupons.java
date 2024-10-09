66110793e332c034228a412092f0e7004697cdae
sirasanisivakrishna <98740411+sirasanisivakrishna@users.noreply.github.com>
2024-10-09 10:57:03 +0530

    Add files via upload

---
+++ Capstone-Eureka-Product-MicroService/Capstone-Eureka-Discount-Server/src/main/java/com/hcl/gl/pojo/Coupons.java
@@ -0,0 +1,72 @@
+package com.hcl.gl.pojo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.NonFinal;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Coupons {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long discountId;
+	private String discountCouponName;
+	private int usersId;
+	private double discountAmount;
+	
+	public Coupons(String discountCouponName, int usersId, double discountAmount) {
+		super();
+		this.discountCouponName = discountCouponName;
+		this.usersId = usersId;
+		this.discountAmount = discountAmount;
+	}
+
+	public Coupons() {
+		super();
+	}
+
+	public long getDiscountId() {
+		return discountId;
+	}
+
+	public void setDiscountId(long discountId) {
+		this.discountId = discountId;
+	}
+
+	public String getDiscountCouponName() {
+		return discountCouponName;
+	}
+
+	public void setDiscountCouponName(String discountCouponName) {
+		this.discountCouponName = discountCouponName;
+	}
+
+	public int getUsersId() {
+		return usersId;
+	}
+
+	public void setUsersId(int usersId) {
+		this.usersId = usersId;
+	}
+
+	public double getDiscountAmount() {
+		return discountAmount;
+	}
+
+	public void setDiscountAmount(double discountAmount) {
+		this.discountAmount = discountAmount;
+	}
+	
+	
+	
+}
